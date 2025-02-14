d0c0451a87887483bd257b4b9c5552de18f2a720
MariaAcevedo11 <macevedos2@eafit.edu.co>
2025-02-14 10:26:07 -0500

    First page

--- property/models.py
+++ property/models.py
@@ -5,11 +5,12 @@ from django.db import models
 class Property(models.Model):
 
     name = models.CharField(max_length= 50)   
-    description = models.CharField(max_length= 300) 
+    description = models.CharField(max_length= 1000) 
+    image = models.ImageField(upload_to= 'property/images', null = True)
     propertyType = models.CharField(max_length= 20)
     price = models.FloatField()
     location = models.URLField()
     socioEconomicStatus = models.CharField(max_length= 1)
-    ID = models.FloatField()
+    identificator = models.FloatField(null = True)
 
 
--- property/views.py
+++ property/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Property
 
 # Create your views here.
+
+def home(request):
+    #return render(request, 'home.html')
+    searchTerm = request.GET.get('searchProperty')
+    if searchTerm: 
+        property = Property.objects.filter(name__icontains = searchTerm)
+    else:
+        property = Property.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'propertys': property})
+   
