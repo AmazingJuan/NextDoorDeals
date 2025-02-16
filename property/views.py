18cfd0cd69b728921556b576a0d194c18c06f523
Juan Avendaño <jpavendanb@eafit.edu.co>
2025-02-16 14:01:42 -0500

    price filters logic

--- property/views.py
+++ property/views.py
@@ -9,13 +9,27 @@ def home(request):
     searchTerm = request.GET.get('searchProperty')
     minPrice = request.GET.get('minPrice')
     maxPrice = request.GET.get('maxPrice')
+    property = Property.objects.all()
+    print(property)
+    if minPrice:
+        minPrice = float(minPrice)
+        property = property.filter(price__gte = minPrice)
+    if maxPrice:
+        maxPrice = float(maxPrice)
+        if minPrice and minPrice <= maxPrice:
+            property = property.filter(price__lte = maxPrice)
+        elif not minPrice:
+            property = property.filter(price__lte = maxPrice)
+
 
     if searchTerm: 
-        property = Property.objects.filter(name__icontains = searchTerm)
+        property = property.filter(name__icontains = searchTerm)
     else:
-        property = Property.objects.all()
+        searchTerm = None
 
-    return render(request, 'home.html', {'searchTerm': searchTerm, 'propertys': property, 'minPrice': minPrice, 'maxPrice': maxPrice})
+    propertyPresence = True if property.count() > 0 else False
+    
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'propertys': property, 'minPrice': minPrice, 'maxPrice': maxPrice, 'propertyPresence' : propertyPresence})
  
 
    
