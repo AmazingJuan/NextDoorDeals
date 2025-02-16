6d4db0319b1582652f2234394cda01fb418ee280
Juan Avendaño <jpavendanb@eafit.edu.co>
2025-02-16 14:49:39 -0500

    socioeconomic filter

--- property/views.py
+++ property/views.py
@@ -3,33 +3,49 @@ from django.http import HttpResponse
 from .models import Property
 
 # Create your views here.
+def filterPrice(minAttr, maxAttr, property):
+    if minAttr:
+        minAttr = float(minAttr)
+        print(minAttr)
+        property = property.filter(price__gte = minAttr)
+    print(property)
+    if maxAttr:
+        maxAttr = float(maxAttr)
+        if minAttr and minAttr <= maxAttr:
+            property = property.filter(price__lte = maxAttr)
+        elif not minAttr:
+            property = property.filter(price__lte = maxAttr)
+    return property
+
+def filterSES(minAttr, maxAttr, property):
+    if minAttr:
+        minAttr = int(minAttr)
+        property = property.filter(socioEconomicStatus__gte = minAttr)
+    if maxAttr:
+        maxAttr = int(maxAttr)
+        if minAttr and minAttr <= maxAttr:
+            property = property.filter(socioEconomicStatus__lte = maxAttr)
+        elif not minAttr:
+            property = property.filter(socioEconomicStatus__lte = maxAttr)
+    return property
 
 def home(request):
-
     searchTerm = request.GET.get('searchProperty')
     minPrice = request.GET.get('minPrice')
     maxPrice = request.GET.get('maxPrice')
-    property = Property.objects.all()
-    print(property)
-    if minPrice:
-        minPrice = float(minPrice)
-        property = property.filter(price__gte = minPrice)
-    if maxPrice:
-        maxPrice = float(maxPrice)
-        if minPrice and minPrice <= maxPrice:
-            property = property.filter(price__lte = maxPrice)
-        elif not minPrice:
-            property = property.filter(price__lte = maxPrice)
-
-
+    minSES = request.GET.get('minSES')
+    maxSES = request.GET.get('maxSES')
+    print(minSES, maxSES)
+    properties = Property.objects.all()
+    properties = filterPrice(minPrice, maxPrice, properties)
+    properties = filterSES(minSES, maxSES, properties)
+    
     if searchTerm: 
-        property = property.filter(name__icontains = searchTerm)
+        properties = properties.filter(name__icontains = searchTerm)
     else:
         searchTerm = None
 
-    propertyPresence = True if property.count() > 0 else False
+    propertyPresence = True if properties.count() > 0 else False
     
-    return render(request, 'home.html', {'searchTerm': searchTerm, 'propertys': property, 'minPrice': minPrice, 'maxPrice': maxPrice, 'propertyPresence' : propertyPresence})
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'propertys': properties, 'minPrice': minPrice, 'maxPrice': maxPrice, 'propertyPresence' : propertyPresence})
  
-
-   
