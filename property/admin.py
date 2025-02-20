72b5f9c317cf5d2b16fb41f8f0a99f6bcbe9dc8c
Juan Avendaño <jpavendanb@eafit.edu.co>
2025-02-19 22:57:41 -0500

    logo fix & admin registration

--- property/admin.py
+++ property/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Property
+from .models import City
+from .models import Province
+from .models import Neighborhood
+from .models import Address
+from .models import District
 # Register your models here.
 
 admin.site.register(Property)
+admin.site.register(City)
+admin.site.register(Province)
+admin.site.register(Neighborhood)
+admin.site.register(Address)
+admin.site.register(District)
+
