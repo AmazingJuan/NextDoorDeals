d457914a92d4891d3e345729e5e171a40c4b16b4
MariaAcevedo11 <macevedos2@eafit.edu.co>
2025-03-13 11:34:10 -0500

    Better SocioEconomicStatus

--- property/views.py
+++ property/views.py
@@ -18,16 +18,20 @@ def filterPrice(minAttr, maxAttr, property):
             property = property.filter(price__lte = maxAttr)
     return property
 
-def filterSES(minAttr, maxAttr, property):
-    if minAttr:
-        minAttr = int(minAttr)
-        property = property.filter(socioEconomicStatus__gte = minAttr)
-    if maxAttr:
-        maxAttr = int(maxAttr)
-        if minAttr and minAttr <= maxAttr:
-            property = property.filter(socioEconomicStatus__lte = maxAttr)
-        elif not minAttr:
-            property = property.filter(socioEconomicStatus__lte = maxAttr)
+def filterSES(minAttr, maxAttr, property, isChecked, attr):
+    if isChecked and attr:
+        attr = int(attr)
+        property = property.filter(socioEconomicStatus__exact=attr)
+    else:
+        if minAttr:
+            minAttr = int(minAttr)
+            property = property.filter(socioEconomicStatus__gte = minAttr)
+        if maxAttr:
+            maxAttr = int(maxAttr)
+            if minAttr and minAttr <= maxAttr:
+                property = property.filter(socioEconomicStatus__lte = maxAttr)
+            elif not minAttr:
+                property = property.filter(socioEconomicStatus__lte = maxAttr)
     return property
 
 def filterDistrict(district, properties):
@@ -41,23 +45,34 @@ def home(request):
     minSES = request.GET.get('minSES')
     maxSES = request.GET.get('maxSES')
     district = request.GET.get('district')
-    print(minSES, maxSES)
+
+    # Capturar valores faltantes para filtro SES
+    isChecked = request.GET.get('useRangeSES', 'off') == 'on'  # Verifica si el checkbox está marcado
+    attr = request.GET.get('singleSES', None)  # Estrato único si el checkbox está activado
+
+    print(minSES, maxSES, isChecked, attr)  # Para depuración
+
     properties = Property.objects.all()
     properties = filterPrice(minPrice, maxPrice, properties)
-    properties = filterSES(minSES, maxSES, properties)
+    properties = filterSES(minSES, maxSES, properties, isChecked, attr)  # Ahora los parámetros están definidos
     cities = City.objects.all()
     districts = District.objects.all()
-    
+
     if searchTerm: 
-        properties = properties.filter(name__icontains = searchTerm)
+        properties = properties.filter(name__icontains=searchTerm)
     else:
         searchTerm = None
 
     if district:
         properties = filterDistrict(district, properties)
 
-    propertyPresence = True if properties.count() > 0 else False
-    
-    return render(request, 'home.html', {'searchTerm': searchTerm, 
-        'propertys': properties, 'minPrice': minPrice, 'maxPrice': maxPrice, 
-        'propertyPresence' : propertyPresence, 'districts': districts})
+    propertyPresence = properties.exists()  # Mejor alternativa a count() > 0
+
+    return render(request, 'home.html', {
+        'searchTerm': searchTerm,
+        'propertys': properties,
+        'minPrice': minPrice,
+        'maxPrice': maxPrice,
+        'propertyPresence': propertyPresence,
+        'districts': districts
+    })
